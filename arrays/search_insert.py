552e362bb74b60154e46b699385ee2db21a22480
aazambrano2 <aazambrano2@miners.utep.edu>
2021-04-03 18:08:36 -0600

    problems invoving array(s)

---
+++ arrays/search_insert.py
@@ -0,0 +1,14 @@
+'''
+Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+Source: https://leetcode.com/problems/search-insert-position/
+'''
+def search_insert( nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+if __name__ == '__main__':
+    pass
