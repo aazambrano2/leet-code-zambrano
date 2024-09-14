711a0ba4e5e2bae4be8695c3f3040c7767200873
aazambrano2 <aazambrano2@miners.utep.edu>
2024-09-14 00:49:25 -0600

    added optimal solution for this problem

--- product__of_array_discluding_self.py
+++ product__of_array_discluding_self.py
@@ -49,4 +49,19 @@ class Solution:
 
 
 
+    """
+
+    Better optimized solution
+
+    answer = [1] * (len(nums))
+
+    for i in range(1, len(nums)):
+        answer[i] = answer[i-1] * nums[i-1]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        answer[i] *= postfix
+        postfix *= nums[i]
+    return answer
+    """
+
         
