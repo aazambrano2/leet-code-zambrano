173fc9daad0d15147727cc9a7a590bc70a64cc60
aazambrano2 <aazambrano2@miners.utep.edu>
2024-10-08 12:14:46 -0600

    two sum part 2 solution

---
+++ two_sum_II.py
@@ -0,0 +1,66 @@
+class Solution:
+    '''
+        Input: numbers = [1,2,3,4], target = 3
+
+        Output: [1,2]
+
+        [1,2,3,4]
+        t
+        t - numbers[0] = 2 : 0
+        ....
+        t - numbsers[3] = -1: 3
+
+        if t - numbers[0] in dict():
+            return [i, dict[3 - numbers[0]]]
+        
+        No hashmap
+         3-1=2
+         x y
+         compare x < y and numbers[x] + numbers[y] = target
+             return [x+1,y+1]
+         else:
+            y += 1
+        if y = len(numbers):
+            update x
+            x += 1
+            y = 0
+        while conditions will be if x < len(numbers)
+
+        [1,2,3,4]
+
+
+
+    '''
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        x=0
+        y=1
+        
+        while(x < len(numbers)):
+            if x < y and x != y and numbers[x] + numbers[y] == target:
+                return [x+1,y+1]
+            else:
+                y += 1
+            
+            if y == len(numbers):
+                x += 1
+                y = 0
+        
+        return []
+
+        '''
+
+        class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+        '''
